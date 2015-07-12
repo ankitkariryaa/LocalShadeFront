8e5bd399061a545c5f1f366c6ff186ab5b74e5b2
unknown <ankit.ky@gmail.com>
2015-07-12 10:23:44 +0300

    Display markers and polygons from array

--- public/js/Directives.js
+++ public/js/Directives.js
@@ -1,1 +1,21 @@
-Directive.js
+app.directive('lotOfPolygon',function(){
+return{
+restrict: 'E',
+scope: {
+},
+  templateUrl: 'js/directives/lotOfPolygon.html',
+  link: function(scope, element, attrs) { 
+  scope.buttonText = "Install", 
+  scope.installed = false, 
+
+  scope.download = function() { 
+    element.toggleClass('btn-active'); 
+    if(scope.installed) { 
+      scope.buttonText = "Install"; 
+      scope.installed = false; 
+    } else { 
+      scope.buttonText = "Uninstall"; 
+      scope.installed = true; 
+    } 
+  } 
+}
